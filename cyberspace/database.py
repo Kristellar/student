ed0180b978f19c7ac138df78d07dc33080990ec8
Debasish Sundaray <debasish@kristellar.com>
2024-08-31 09:06:01 +0530

    third update

--- cyberspace/database.py
+++ cyberspace/database.py
@@ -1,9 +1,17 @@
-
+import configparser
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Shadowtrace#24@localhost/cyberspace_academy"
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+db_user = config['DATABASE']['USER']
+db_password = config['DATABASE']['PASSWORD']
+db_host = config['DATABASE']['HOST']
+db_name = config['DATABASE']['DB_NAME']
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
